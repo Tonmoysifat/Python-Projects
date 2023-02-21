bef854317f43e6eac4e0931e19f8da90c1df4359
Tonmoysifat <98707041+Tonmoysifat@users.noreply.github.com>
2023-02-21 22:42:18 +0600

    Update RN.py

--- RN.py
+++ RN.py
@@ -6,8 +6,8 @@ import time
 print("                                                          Welcome to the Random Number game\n\n")
 time.sleep(3)
 print("                                                       This game is played between two players\n\n")
-Player1=input("  Player 1: ")
-Player2=input("  Player 2: ") 
+Player1=input("   Name of player 1: ")
+Player2=input("   Name of player 2: ") 
 print(f"\n\n                                                         {Player1} vs {Player2}\n")
 import random
 lst=[1,2,3,4,5,6]
@@ -21,21 +21,21 @@ while round<10:
   ran2=random.choice(lst)
   P2_score=P2_score+ran2
   print("  Round:",round)
-  print(f"\n    To activate {Player1}'s turn press 1")
+  print(f"\n    To activate {Player1}'s turn press 1 then Enter")
   press=int(input("     "))
   if press==1:
-    print(f"                   {Player1} score's {ran1}\n")
+    print(f"                   {Player1} scored's {ran1}\n")
   else:
     print("Error")  
-  print(f"    To activate {Player2}'s turn press 2")
+  print(f"    To activate {Player2}'s turn press 2 then Enter")
   press=int(input("     "))
   if press==2:
-    print(f"                   {Player2} score's {ran2}\n")
+    print(f"                   {Player2} scored's {ran2}\n")
     time.sleep(3)
   else:
     print("Error")  
   print(" After Round:",round)
-  print(f"    {Player1} score is {P1_score}\n    {Player2} score is {P2_score}\n")
+  print(f"    {Player1}'s score is {P1_score}\n    {Player2}'s score is {P2_score}\n")
 if P1_score==P2_score:
   print("                                                                     Match Tie")
 elif P1_score>P2_score:
